16d64c281dc0bc9391b2546a05784abddd8e40fa
pavel <saenko359@gmail.com>
2019-08-12 22:52:04 +0300

    Order Admin

--- assets/js/modules/cart.js
+++ assets/js/modules/cart.js
@@ -5,63 +5,63 @@ let addToCartButtons, cartTable;
 addToCartButtons = document.querySelectorAll('.js-add-to-cart');
 
 addToCartButtons.forEach((button) => {
-	button.addEventListener('click', (event) => {
-		event.preventDefault();
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
 
-		fetch(button.href, {
-			headers: {
-				'X-Requested-With': 'XMLHttpRequest'
-			}
-		})
-			.then((response) => {
-				return response.text();
-			})
-			.then((body) => {
-				document.getElementById('header-cart').innerHTML = body;
-			})
-	});
+        fetch(button.href, {
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        })
+            .then((response) => {
+                return response.text();
+            })
+            .then((body) => {
+                document.getElementById('header-cart').innerHTML = body;
+            })
+    });
 });
 
 cartTable = document.getElementById('cartTable');
 
 if (cartTable) {
-	cartTable.addEventListener('input', (event) => {
-		if (!event.target.classList.contains('js-cart-item-count')) {
-			return;
-		}
+    cartTable.addEventListener('input', (event) => {
+        if (!event.target.classList.contains('js-cart-item-count')) {
+            return;
+        }
 
-		let input = event.target;
-		let formData = new FormData();
+        let input = event.target;
+        let formData = new FormData();
 
-		formData.set('count', input.value);
+        formData.set('count', input.value);
 
-		fetch(input.dataset.url, {
-			method: 'post',
-			body: formData
-		})
-			.then((response) => {
-				return response.text();
-			})
-			.then((body) => {
-				cartTable.innerHTML = body;
-			});
-	});
+        fetch(input.dataset.url, {
+            method: 'post',
+            body: formData
+        })
+            .then((response) => {
+                return response.text();
+            })
+            .then((body) => {
+                cartTable.innerHTML = body;
+            });
+    });
 
-	cartTable.addEventListener('click', (event) => {
-		let link = event.target.closest('.js-cart-remove-item');
+    cartTable.addEventListener('click', (event) => {
+        let link = event.target.closest('.js-cart-remove-item');
 
-		if (!link) {
-			return;
-		}
+        if (!link) {
+            return;
+        }
 
-		event.preventDefault();
+        event.preventDefault();
 
-		fetch(link.href)
-			.then((response) => {
-				return response.text();
-			})
-			.then((body) => {
-				cartTable.innerHTML = body;
-			});
-	})
+        fetch(link.href)
+            .then((response) => {
+                return response.text();
+            })
+            .then((body) => {
+                cartTable.innerHTML = body;
+            });
+    })
 }
